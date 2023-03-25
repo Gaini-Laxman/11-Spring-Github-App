bb19dae653e21d8ca694f7464803f8b3be7e88de
Gaini Laxman <gainilaxman12@gmail.com>
2023-03-26 00:11:01 +0530

    hi added

--- DebitCard.java
+++ DebitCard.java
@@ -5,7 +5,7 @@ public DebitCard{
 pubitc static void main(String args[]){
 
 String s = new String();
-String s2 = "Hello Java";  
+String s2 = "Hii Hello Java";  
 System.out.println("DebitCard Class Created")
 }
 }
